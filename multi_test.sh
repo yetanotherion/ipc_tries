158c3f6cf114e6396c9204f25996128f9e0ccffb
Ion Alberdi <nolaridebi@gmail.com>
2014-06-09 16:27:43 +0200

    multi_test.sh: take .h refactoring into account

--- multi_test.sh
+++ multi_test.sh
@@ -1,6 +1,6 @@
 make
 ./erase_sem
-NUM_MASTER=`grep "define NUM_MASTER" multi_test.c | cut -d' ' -f3`
+NUM_MASTER=`grep "define NUM_MASTER" utils.h | cut -d' ' -f3`
 for i in `seq $NUM_MASTER`; do
     ./multi_test &
 done
